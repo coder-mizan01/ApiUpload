eb919aa543dea4d09cc5933264844beb0cdaa804
Sazu <p.mizan2441@gmail.com>
2023-06-06 22:47:08 +0600

    add products page

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/',(req,res)=>{
      res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/Products',(req,res)=>{
+    res.sendFile(__dirname+'/products.js')
+})
+
 app.listen(PORT ,(req,res)=>{
     console.log(`server is running on ${PORT}`);
 })
---
+++ products.js
@@ -0,0 +1,18 @@
+ [
+    {
+        id : 1,
+        title : 'Micro Wave'
+    },
+    {
+        id : 2,
+        title : 'Graphics Card'
+    },
+    {
+        id : 3,
+        title : 'MotherBoard'
+    },
+    {
+        id : 4,
+        title : 'Harry potty'
+    }
+]
