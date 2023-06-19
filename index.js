d80a7434ae16b2af430568af605419d77ef2b92e
Sazu <p.mizan2441@gmail.com>
2023-06-19 22:56:55 +0600

    10th commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 const connectDB = require("./DB/Connect");
 
+app.use(cors());
 
 const products_routes = require("./Routes/products");
+
 
 app.get("/", (req, res) => {
   res.send("Hi, I am live ");
