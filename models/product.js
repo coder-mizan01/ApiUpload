f20dad4d27dc3c84b7247e8fc89ec3f940db9e9c
Sazu <p.mizan2441@gmail.com>
2023-06-25 23:38:55 +0600

    14th commit

--- models/product.js
+++ models/product.js
@@ -54,6 +54,10 @@ const productSchema = new mongoose.Schema({
     type : String,
     required : true,
   },
+  type:{
+    type: String,
+    required : true,
+  },
   price: {
     type: Number,
     required: true,
